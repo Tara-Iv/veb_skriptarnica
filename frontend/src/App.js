ebe469992142e393c64205fcd018af5203f89ee4
Tara-Iv <tara.ivosevic@gmail.com>
2026-04-29 19:20:27 +0200

    Vezbe 5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen'
 import { Container } from 'react-bootstrap'
+
 
 const App = () => {
   return (
@@ -10,7 +12,7 @@ const App = () => {
     <Header />
     <main className="py-3">
     <Container>
-      <HomeScreen />
+      <Outlet />
     </Container>
     </main>
     <Footer />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,15 +6,18 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom'
+import './assets/styles/bootstrap-custom.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />}></Route>
+      <Route path="/product/:id" element={<ProductScreen />}></Route>
     </Route>
   )
 );
